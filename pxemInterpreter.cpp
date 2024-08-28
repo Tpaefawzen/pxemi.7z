2beb031ffc0a056660b32a2a62b7006fbfaf0c1a
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2024-08-28 19:23:17 +0900

    Refactored pxemInterpreter.cpp

--- pxemInterpreter.cpp
+++ pxemInterpreter.cpp
@@ -3,36 +3,35 @@
 #include <string>
 #include "pxem.h"
 
-using namespace std;
-
-
-int print_error(char *hoge,char *hogehoge){
-	cerr << hoge << hogehoge << endl;
-	return -1;
-}
-
 int main(int argc,char **argv){
-	if(argc>1){
-		string fileName = argv[1];
-		ifstream fR(fileName.c_str());
-		if(!fR.is_open())
-			return print_error("FileOpenError: ",(char*)fileName.c_str());
-		string fileContents;
-		char c;
-		while(fR.get(c))
-			fileContents += c;
-		fR.close();
-		string fileName2;
-		if(fileName.rfind('/') == string::npos)
-			if(fileName.rfind('\\') != string::npos)
-				fileName2 = fileName.substr(1+(int)fileName.rfind('\\'));
-			else
-				fileName2 = fileName;
+	if ( argc == 0 ) {
+		std::cerr << "Usage: " << argv[0] << " FILE" << std::endl;
+		return 1;
+	}
+
+	std::string fileName = argv[1];
+	std::string fileName2;
+	std::ifstream fR(fileName.c_str());
+	std::string fileContents;
+
+	if ( !fR.is_open() ) {
+		std::cerr << "FileOpenError: " << fileName << std::endl;
+		goto Run;
+	}
+
+	char c;
+	while(fR.get(c))
+		fileContents += c;
+	fR.close();
+	if(fileName.rfind('/') == std::string::npos)
+		if(fileName.rfind('\\') != std::string::npos)
+			fileName2 = fileName.substr(1+(int)fileName.rfind('\\'));
 		else
-			fileName2 = fileName.substr(1+(int)fileName.rfind('/'));
-		Pxem pxemi((char*)fileName2.c_str(),(char*)fileContents.c_str());
-		pxemi.run();
-	}else
-		return print_error("usage: pxemi pxem","FileName");
-	return 0;
+			fileName2 = fileName;
+	else
+		fileName2 = fileName.substr(1+(int)fileName.rfind('/'));
+
+Run:
+	Pxem pxemi((char*)fileName2.c_str(),(char*)fileContents.c_str());
+	pxemi.run();
 }
